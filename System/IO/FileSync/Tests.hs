d0c2c091e1d82a1208b53327c79541923476364c
Janos Tapolczai <janos.tapolczai@gmail.com>
2016-01-03 21:22:02 +0100

    Added separate tests for createFileTree and createDiffTree.

--- System/IO/FileSync/Tests.hs
+++ System/IO/FileSync/Tests.hs
@@ -18,6 +18,8 @@ runTests = hspec . fromHUnitTest $ tests
 tests = TestList
    [TestLabel "createEmptyFileTree" $ TestCase createEmptyFileTree,
     TestLabel "createFileTree1" $ TestCase createFileTree1,
+    TestLabel "createEmptyDiffTree" $ TestCase createEmptyDiffTree,
+    TestLabel "createDiffTree1" $ TestCase createDiffTree1,
     TestLabel "sortTree1" $ TestCase sortTree1,
     TestLabel "sortTree2" $ TestCase sortTree2,
     TestLabel "sortTree3" $ TestCase sortTree3,
@@ -25,6 +27,9 @@ tests = TestList
     TestLabel "sortTree5" $ TestCase sortTree5,
     TestLabel "sortTree6" $ TestCase sortTree6,
     TestLabel "sortTree7" $ TestCase sortTree7]
+
+-- Create file tree
+-------------------------------------------------------------------------------
 
 createEmptyFileTree :: Assertion
 createEmptyFileTree =
@@ -35,6 +40,40 @@ createEmptyFileTree =
 
 createFileTree1 :: Assertion
 createFileTree1 =
+   bracket' (do mkD "testDir/dir1"
+                mkD "testDir/dir1/both"
+                mkD "testDir/dir1/both/both1"
+                mkF "testDir/dir1/both/both1" "both2.txt" "both2_left"
+                mkD "testDir/dir1/both/both_onlyL"
+                mkF "testDir/dir1/both/both_onlyL" "both_onlyL.txt" "both_onlyL"
+                mkF "testDir/dir1/both/" "both.txt" "both"
+                mkD "testDir/dir1/onlyL"
+                mkD "testDir/dir1/onlyL/l1"
+                mkD "testDir/dir1/onlyL/l1/l3"
+                mkD "testDir/dir1/onlyL/l1/l3/l4"
+                mkF "testDir/dir1/onlyL/l1/l3/l4" "l2txt.txt" "l2"
+                mkF "testDir/dir1/onlyL/l1" "ltxt.txt" "ltxt"
+                mkD "testDir/dir1/onlyL/l2")
+           (rmD "testDir")
+           (do t <- createFileTree (LR "testDir/dir1")
+               putStrLn "Expected forest:"
+               putStrLn . Tr.drawForest . map (fmap show) . sortForest $ ft1 
+               putStrLn "Actual forest:"
+               putStrLn . Tr.drawForest . map (fmap show) . sortForest $ t 
+               assertEqual "file tree 1" (sortForest ft1) (sortForest t))
+
+-- Create diff tree
+-------------------------------------------------------------------------------
+
+createEmptyDiffTree :: Assertion
+createEmptyDiffTree =
+   bracket' (mkD "testDir/empty")
+            (rmD "testDir")
+            (do t <- createDiffTree (LR "testDir/empty") (RR "testDir/empty")
+                assertEqual "empty file tree" [] t)
+
+createDiffTree1 :: Assertion
+createDiffTree1 =
    bracket' (do mkD "testDir/dir1"
                 mkD "testDir/dir1/both"
                 mkD "testDir/dir1/both/both1"
@@ -65,10 +104,13 @@ createFileTree1 =
            (rmD "testDir")
            (do t <- createDiffTree (LR "testDir/dir1") (RR "testDir/dir2")
                putStrLn "Expected forest:"
-               putStrLn . Tr.drawForest . map (fmap show) . sortForest $ dt 
+               putStrLn . Tr.drawForest . map (fmap show) . sortForest $ dt1
                putStrLn "Actual forest:"
                putStrLn . Tr.drawForest . map (fmap show) . sortForest $ t 
-               assertEqual "file tree 1" (sortForest dt) (sortForest t))
+               assertEqual "diff tree 1" (sortForest dt1) (sortForest t))
+
+-- Sort forest
+-------------------------------------------------------------------------------
 
 sortTree1 :: Assertion
 sortTree1 = assertEqual "empty forests" (sortForest fr) (sortForest fr)
@@ -81,7 +123,7 @@ sortTree2 = assertBool "one empty and one non-empty forest"
                        $ sortForest [Tr.Node 1 []] /= sortForest []
 
 sortTree3 :: Assertion
-sortTree3 = assertEqual "the same forest twice" (sortForest dt) (sortForest dt)
+sortTree3 = assertEqual "the same forest twice" (sortForest dt1) (sortForest dt1)
 
 sortTree4 :: Assertion
 sortTree4 = assertEqual "equal forests mod. sorting" (sortForest [tr1]) (sortForest [tr2])
@@ -98,27 +140,42 @@ sortTree7 = assertBool "different trees (even mod. sorting)" $ sortForest [tr1] 
 -- Test data
 -------------------------------------------------------------------------------
 
-dt :: Tr.Forest (TreeDiff, FilePath)
-dt = [Tr.Node (Both, "both")
-        [Tr.Node (Both, "both1")
-           [Tr.Node (Both, "both2.txt") []],
-         Tr.Node (LeftOnly, "both_onlyL")
-            [Tr.Node (LeftOnly, "both_onlyL.txt") []],
-         Tr.Node (RightOnly, "both_onlyR")
-            [Tr.Node (RightOnly, "both_onlyR.txt") []],
-         Tr.Node (Both, "both.txt") []],
-      Tr.Node (LeftOnly, "onlyL")
-         [Tr.Node (LeftOnly, "l1")
-            [Tr.Node (LeftOnly, "l3")
-               [Tr.Node (LeftOnly, "l4")
-                  [Tr.Node (LeftOnly, "l2txt.txt") []]],
-             Tr.Node (LeftOnly, "ltxt.txt") []],
-          Tr.Node (LeftOnly, "l2") []],
-      Tr.Node (RightOnly, "onlyR")
-         [Tr.Node (RightOnly, "r1") [],
-          Tr.Node (RightOnly, "r2")
-            [Tr.Node (RightOnly, "r3")
-               [Tr.Node (RightOnly, "rtxt.txt") []]]]]
+ft1 :: Tr.Forest FilePath
+ft1 = [Tr.Node ("both")
+         [Tr.Node ("both1")
+            [Tr.Node ("both2.txt") []],
+          Tr.Node ("both_onlyL")
+             [Tr.Node ("both_onlyL.txt") []],
+          Tr.Node ("both.txt") []],
+       Tr.Node ("onlyL")
+          [Tr.Node ("l1")
+             [Tr.Node ("l3")
+                [Tr.Node ("l4")
+                   [Tr.Node ("l2txt.txt") []]],
+              Tr.Node ("ltxt.txt") []],
+           Tr.Node ("l2") []]]
+
+dt1 :: Tr.Forest (TreeDiff, FilePath)
+dt1 = [Tr.Node (Both, "both")
+         [Tr.Node (Both, "both1")
+            [Tr.Node (Both, "both2.txt") []],
+          Tr.Node (LeftOnly, "both_onlyL")
+             [Tr.Node (LeftOnly, "both_onlyL.txt") []],
+          Tr.Node (RightOnly, "both_onlyR")
+             [Tr.Node (RightOnly, "both_onlyR.txt") []],
+          Tr.Node (Both, "both.txt") []],
+       Tr.Node (LeftOnly, "onlyL")
+          [Tr.Node (LeftOnly, "l1")
+             [Tr.Node (LeftOnly, "l3")
+                [Tr.Node (LeftOnly, "l4")
+                   [Tr.Node (LeftOnly, "l2txt.txt") []]],
+              Tr.Node (LeftOnly, "ltxt.txt") []],
+           Tr.Node (LeftOnly, "l2") []],
+       Tr.Node (RightOnly, "onlyR")
+          [Tr.Node (RightOnly, "r1") [],
+           Tr.Node (RightOnly, "r2")
+             [Tr.Node (RightOnly, "r3")
+                [Tr.Node (RightOnly, "rtxt.txt") []]]]]
 
 tr1 :: Tr.Tree Int
 tr1 = Tr.Node 1 [l 4, l 3, l 1, l 8, Tr.Node 7 [l 1, l 9]]

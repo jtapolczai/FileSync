0b080929221ca779662980ed2577bf71d8ebc147
Janos Tapolczai <janos.tapolczai@gmail.com>
2016-01-10 00:14:01 +0100

    Rewrote genericJoin to handle join conflicts.

--- System/IO/FileSync/Join.hs
+++ System/IO/FileSync/Join.hs
@@ -1,47 +1,71 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 
 module System.IO.FileSync.Join where
 
-import Control.Arrow ((&&&))
+import qualified Data.Foldable as F
+import Data.List
 import qualified Data.Map as M
+import qualified Data.Sequence as S
+import qualified Data.Set as St
 import qualified Data.Tree as T
 
 import System.IO.FileSync.Types
 
 -- |Generic join that computer the set of differences between two forests.
 --  Ordering of subtrees is __not__ guaranteed. Subtrees with identical
---  root trees are merged.
+--  roots (according to '==') are merged.
 genericJoin
-   :: (Ord a, Show a)
+   :: forall a.(LooseEq a, Ord a, Ord (Reduct a), Show a, Ord (T.Tree a))
    => T.Forest a -- ^The left forest S.
    -> T.Forest a -- ^The right forest T.
-   -> T.Forest (a, TreeDiff)
-genericJoin ss ts =
-   map (terminate LeftOnly) leftOnly
-   ++ map (terminate RightOnly) rightOnly
-   ++ map continue both
+   -> T.Forest (St.Set a, TreeDiff) -- ^Joined forest. Values equal according to '=~='
+                                    --  but __not__ according to '==' are grouped.
+genericJoin ss ts = 
+   map snd . M.toList . fmap recurse . groupChildren $ rawChildren
    where
-      toMap = M.fromListWith (\x y -> fmap fst $ head $ genericJoin [x] [y])
-              . map (T.rootLabel &&& id)
+      rawChildren = map (,LeftOnly) ss ++ map (,RightOnly) ts
 
-      -- convert lists of children to maps for effient difference/intersection
-      ssMap = toMap ss
-      tsMap = toMap ts
+      groupChildren :: [(T.Tree a, TreeDiff)]
+                    -> EqClasses (Reduct a) (T.Tree a, TreeDiff)
+      groupChildren = foldl' f $ M.empty
+         where
+            f acc val =
+               M.insertWith (\_ old -> St.insert val old)
+                            (reduce . T.rootLabel . fst $ val)
+                            (St.singleton val)
+                            acc
 
-      -- recurse for all keys that occur in both forests.
-      -- end by inserting leftTag/rightTag for those which only occur in one.
-      rightOnly = map snd $ M.toList $ M.difference tsMap ssMap
-      leftOnly = map snd $ M.toList $ M.difference ssMap tsMap
-      both = map snd $ M.toList $ M.intersectionWith pairChildren ssMap tsMap
+      recurse :: St.Set (T.Tree a, TreeDiff) -> T.Tree (St.Set a, TreeDiff)
+      recurse = mkNode . F.foldl' f (St.empty, Both, S.empty, S.empty)
+         where
+            mkNode (x, side, ls, rs) =
+               T.Node (x, side) $ genericJoin (F.toList ls) (F.toList rs) 
 
-      pairChildren (T.Node x xs) (T.Node _ ys) = (x,xs,ys)
-
-      continue (x,xs,ys) = T.Node (x, Both) $ genericJoin xs ys
-      terminate tag (T.Node x xs) = T.Node (x, tag) $ map (fmap (,tag)) xs
+            f (vals, accSide, ls, rs) (T.Node x xs, side) =
+               (St.insert x vals,
+                if accSide == side then side else Both,
+                if side == LeftOnly then S.fromList xs S.>< ls else ls,
+                if side == RightOnly then S.fromList xs S.>< rs else rs)
 
 -- |Filters out all sub-trees whose roots fail a predicate.
 filterForest :: (a -> Bool) -> T.Forest a -> T.Forest a
 filterForest pred = map filterForest' . filter (pred . T.rootLabel)
    where
       filterForest' (T.Node x xs) = T.Node x $ filterForest pred xs
+
+-- |Applies a function to every node of a forest. Iff the function fails
+--  for any node of any tree, Nothing is returned.
+reforest :: (a -> Maybe b) -> T.Forest a -> Maybe (T.Forest b)
+reforest f = mapM (traverse f)
+
+-- |Returns Nothing iff the length of the 'FileTreeData'-list is @/=1@,
+--  which indicates that the same entry names signifies a file in one tree and
+--  a directory in another, meaning that the synching can't proceed.
+
+-- |Tries to extract the first element of the list. Fails if the list
+--  contains anything else than exactly 1 element.
+onlyOneKey :: Eq a => ([a], b) -> Maybe (a, b)
+onlyOneKey (xs,y) = if take 1 xs /= xs then Nothing else Just $ (head xs, y)
